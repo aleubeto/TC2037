e8d2e4ef83b00529726edc77b1166aad8e9e2524
ale_local <alejo.ubeto@gmail.com>
2022-02-25 17:54:56 -0600

    BOWLING: programacion del Bowling game

--- bowling/test/bowling_test.exs
+++ bowling/test/bowling_test.exs
@@ -1,8 +1,9 @@
 defmodule BowlingTest do
   use ExUnit.Case
-  doctest Bowling
 
-  test "greets the world" do
-    assert Bowling.hello() == :world
+  test "butter game" do
+    game = List.duplicate([0,0],9) ++ [[0,0, nil]]
+    assert Bowling.score(game) == 0
   end
+  
 end
