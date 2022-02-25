6787de4bb84505560efc1814bfbeff13b809c9a8
ale_local <alejo.ubeto@gmail.com>
2022-02-25 17:52:15 -0600

    EXAMPLE: Implementacion de funciones: factorial y reverse

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,18 +1,11 @@
 defmodule Example do
-  @moduledoc """
-  Documentation for `Example`.
-  """
 
-  @doc """
-  Hello world.
+  #función para calcular factorial de un número
+  def factorial(0), do: 1
+  def factorial(n) when n > 0, do: n * factorial(n - 1)
 
-  ## Examples
+  #función para invertir una lista
+  def reverse([], acc), do: acc
+  def reverse([cabeza|cola], acc), do: reverse(cola, [cabeza|acc])
 
-      iex> Example.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
