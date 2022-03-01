ae24f7847baab16b191f69eb34c00fb1611553bd
ale_local <alejo.ubeto@gmail.com>
2022-03-01 12:13:26 -0600

    TEST: butter game, all ones, one spare

--- bowling/lib/bowling.ex
+++ bowling/lib/bowling.ex
@@ -1,5 +1,15 @@
 defmodule Bowling do
-  def score(_game) do
+
+  def score([]) do
     0
   end
+
+  def score([[primer,segundo|_]|resto]) when primer + segundo == 10 do
+    primer + segundo + hd(hd(resto)) + score(resto)
+  end
+
+  def score([[primer,segundo|_]|resto]) do
+    primer + segundo + score(resto)
+  end
+  
 end
--- bowling/test/bowling_test.exs
+++ bowling/test/bowling_test.exs
@@ -5,5 +5,15 @@ defmodule BowlingTest do
     game = List.duplicate([0,0],9) ++ [[0,0, nil]]
     assert Bowling.score(game) == 0
   end
+
+  test "'all ones' game" do
+    game = List.duplicate([1, 1], 9) ++ [[1, 1, nil]]
+    assert Bowling.score(game) == 20
+  end
+
+  test "one spare" do
+    game = [[5,5],[3,0]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 16
+  end
   
 end
