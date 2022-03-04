cb03ea80835969a72ef3c5bffc67416aa1c3f315
ale_local <alejo.ubeto@gmail.com>
2022-03-04 12:56:37 -0600

    DNA: Implementacion de funcion 'count'

--- dna_analysis/lib/dna_analysis.ex
+++ dna_analysis/lib/dna_analysis.ex
@@ -1,2 +1,14 @@
 defmodule DnaAnalysis do
+
+    def count(seq, char) do
+        #count(seq, char, 0)
+        Enum.filter(seq, fn e -> e == char end) |> length()
+    end
+
+    #defp count([], _char, acc), do: acc
+
+    #defp count([char|b], char, acc), do: count(b, char, acc + 1)
+
+    #defp count([_a|b], char, acc), do: count(b, char, acc)
+
 end
