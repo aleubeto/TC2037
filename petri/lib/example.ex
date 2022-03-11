df7faccddae8693f47c36ae83d2a84bc28df3499
ale_local <alejo.ubeto@gmail.com>
2022-03-11 11:39:01 -0600

    Creación de proyecto Petri

---
+++ petri/lib/example.ex
@@ -0,0 +1,36 @@
+defmodule Example do
+  def ex1l do
+    [
+      [P0, A],
+      [P1, B],
+      [P1, D],
+      [P2, C],
+      [P2, D],
+      [P3, E],
+      [P4, E],
+      [A, P1],
+      [A, P2],
+      [B,P3],
+      [C,P4],
+      [D,P3],
+      [D,P4],
+      [E,P5]
+    ]
+  end
+
+  def ex1m do
+    %{
+      P0 => MapSet.new([A]),
+      P1 => MapSet.new([B,D]),
+      P2 => MapSet.new([C,D]),
+      P3 => MapSet.new([E]),
+      P4 => MapSet.new([E]),
+      P5 => MapSet.new([]),
+      A => MapSet.new([P1,P2]),
+      B => MapSet.new([P3]),
+      C => MapSet.new([P4]),
+      D => MapSet.new([P3,P4]),
+      E => MapSet.new([P5])
+    }
+  end
+end
---
+++ petri/lib/petri_l.ex
@@ -0,0 +1,13 @@
+defmodule Petri do
+  def preset(f, n) do
+    f |>
+    Enum.filter(fn[_src, tgt] -> tgt == n end) |>
+    Enum.map(fn [src, _] -> src end)
+  end
+
+  def postset(f,n) do
+    f |>
+    Enum.filter(fn[src, _tgt] -> src == n end) |>
+    Enum.map(fn [_src, tgt] -> tgt end)
+  end
+end
