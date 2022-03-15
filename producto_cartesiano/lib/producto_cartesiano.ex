e3d8546c96c554fcde5345cd7e85915eb13a04d9
ale_local <alejo.ubeto@gmail.com>
2022-03-15 12:14:25 -0600

    Creacion de funcion recursiva

--- producto_cartesiano/lib/producto_cartesiano.ex
+++ producto_cartesiano/lib/producto_cartesiano.ex
@@ -1,18 +1,32 @@
 defmodule ProductoCartesiano do
-  @moduledoc """
-  Documentation for `ProductoCartesiano`.
-  """
 
-  @doc """
-  Hello world.
+# FUNCIÓN PRINCIPAL 'prod'
 
-  ## Examples
+  # delcaración de caso base
+  def prod([], _), do: nil
 
-      iex> ProductoCartesiano.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  # declaración de función recursiva
+  def prod([h|t], conj) do
+    IO.inspect prod1(h, conj, [])
+    prod(t, conj)
   end
+
+# FUNCIÓN ANIDADA 'prod1'
+
+  # declaración de caso base, fin de ejecución
+  def prod1(_, [], acc), do: acc
+
+  # declaración de función recursiva
+  def prod1(elem, [h|t], acc) do
+    IO.puts("#{elem}, #{h}")
+    prod1(elem, t, [{elem, h} | acc])
+  end
+
+# FOR
+  def pr(a, b) do
+    for e1 <- a, e2 <- b do
+      {e1, e2}
+    end
+  end
+
 end
