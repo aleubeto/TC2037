42f99b16ee3c688adefa91e4a8d3b791a808afcd
ale_local <alejo.ubeto@gmail.com>
2022-03-15 11:30:12 -0600

    Creación de proyecto 'producto cartesiano'

---
+++ producto_cartesiano/lib/producto_cartesiano.ex
@@ -0,0 +1,18 @@
+defmodule ProductoCartesiano do
+  @moduledoc """
+  Documentation for `ProductoCartesiano`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProductoCartesiano.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ producto_cartesiano/test/producto_cartesiano_test.exs
@@ -0,0 +1,8 @@
+defmodule ProductoCartesianoTest do
+  use ExUnit.Case
+  doctest ProductoCartesiano
+
+  test "greets the world" do
+    assert ProductoCartesiano.hello() == :world
+  end
+end
