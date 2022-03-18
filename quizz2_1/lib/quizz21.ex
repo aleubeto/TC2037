6e74063e8846e6688a1d8f168e3eb6b260e43bc8
ale_local <alejo.ubeto@gmail.com>
2022-03-17 21:12:00 -0600

    Creación de proyecto quizz 2.1

---
+++ quizz2_1/lib/quizz21.ex
@@ -0,0 +1,18 @@
+defmodule Quizz21 do
+  @moduledoc """
+  Documentation for `Quizz21`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Quizz21.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ quizz2_1/test/quizz21_test.exs
@@ -0,0 +1,8 @@
+defmodule Quizz21Test do
+  use ExUnit.Case
+  doctest Quizz21
+
+  test "greets the world" do
+    assert Quizz21.hello() == :world
+  end
+end
