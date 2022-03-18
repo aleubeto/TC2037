860c0259357b22d2000d27bab47c0a9c4374c422
ale_local <alejo.ubeto@gmail.com>
2022-03-17 22:22:21 -0600

    QUIZZ 2.1: Pregunta 2, done.

--- quizz2_1/lib/quizz21.ex
+++ quizz2_1/lib/quizz21.ex
@@ -2,7 +2,7 @@ defmodule Quizz21 do
 
 # Pregunta 1: occurs
 
-  # Si se recibe una lista llena, se ejecuta el recorrido con contador
+  # F.P: Si se recibe una lista llena, se ejecuta el recorrido con contador
   def occurs(list, char), do: occurs(list, char, 0)
   
   # Recorrido con contador
@@ -14,6 +14,17 @@ defmodule Quizz21 do
   def occurs([], _char, acc), do: acc
 
 # Pregunta 2
+
+  # F.P: Si se recibe una lista con elementos, se ejecuta el llenado de la nueva lista "[]"
+  def remove_at(lista, i), do: remove_at(lista, i, [], 0)
+
+  # Llenado de lista sin el elemento "i"
+  def remove_at([hd|tl], i, nueva, acc) do
+    if (i == acc), do: remove_at([], i, nueva ++ tl, acc), else: remove_at(tl, i, nueva ++ [hd], acc + 1) 
+  end
+
+  # Caso base: Si se recibe una lista vacía, retorna la misma lista vacía
+  def remove_at([], _i, nueva, _acc), do: nueva
 
 # Pregunta 3
 
--- quizz2_1/test/quizz21_test.exs
+++ quizz2_1/test/quizz21_test.exs
@@ -10,4 +10,12 @@ defmodule Quizz21Test do
     assert Quizz21.occurs([2,3,5,7,11,13,7,5], 99) == 0
   end
 
+  #tag :skip
+  test "remove_at" do
+    assert Quizz21.remove_at([1,2,3], 0) ==  [2,3]
+    assert Quizz21.remove_at([4,5,6,7], 2) ==  [4,5,7]
+    assert Quizz21.remove_at([4,5,6,7], 10) ==  [4,5,6,7]
+    assert Quizz21.remove_at([4,5,6,7], -1) ==  [4,5,6,7]
+  end
+
 end
