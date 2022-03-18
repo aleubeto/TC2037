db5a87129f3aef52043d9d57efabf7d2b10c626f
ale_local <alejo.ubeto@gmail.com>
2022-03-17 21:49:56 -0600

    QUIZZ_2.1: Pregunta 1: occurs, done.

--- quizz2_1/lib/quizz21.ex
+++ quizz2_1/lib/quizz21.ex
@@ -1,18 +1,20 @@
 defmodule Quizz21 do
-  @moduledoc """
-  Documentation for `Quizz21`.
-  """
 
-  @doc """
-  Hello world.
+# Pregunta 1: occurs
 
-  ## Examples
-
-      iex> Quizz21.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  # Si se recibe una lista llena, se ejecuta el recorrido con contador
+  def occurs(list, char), do: occurs(list, char, 0)
+  
+  # Recorrido con contador
+  def occurs([hd|tl], char, acc) do
+    if (hd == char), do: occurs(tl, char, acc + 1), else: occurs(tl, char, acc)
   end
+  
+  # Caso base: Si se recibe una lista vacía, retorna 0
+  def occurs([], _char, acc), do: acc
+
+# Pregunta 2
+
+# Pregunta 3
+
 end
--- quizz2_1/test/quizz21_test.exs
+++ quizz2_1/test/quizz21_test.exs
@@ -2,7 +2,12 @@ defmodule Quizz21Test do
   use ExUnit.Case
   doctest Quizz21
 
-  test "greets the world" do
-    assert Quizz21.hello() == :world
+  #@tag :skip
+  test "occurs: counts the number of occurrences of an element in a list" do
+    assert Quizz21.occurs([2,3,5,7,11,13,7,5], 11) == 1
+    assert Quizz21.occurs([2,3,5,7,11,13,7,5], 7) == 2
+    assert Quizz21.occurs([2,3,5,7,11,13,7,5], 5) == 2
+    assert Quizz21.occurs([2,3,5,7,11,13,7,5], 99) == 0
   end
+
 end
