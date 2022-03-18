81f349aa1573de2a217293eda42548a5474de1ef
ale_local <alejo.ubeto@gmail.com>
2022-03-17 23:02:49 -0600

    QUIZZ 2.1: encode_dup, done.

--- quizz2_1/lib/quizz21.ex
+++ quizz2_1/lib/quizz21.ex
@@ -13,7 +13,7 @@ defmodule Quizz21 do
   # Caso base: Si se recibe una lista vacía, retorna 0
   def occurs([], _char, acc), do: acc
 
-# Pregunta 2
+# Pregunta 2: remove_at
 
   # F.P: Si se recibe una lista con elementos, se ejecuta el llenado de la nueva lista "[]"
   def remove_at(lista, i), do: remove_at(lista, i, [], 0)
@@ -26,6 +26,17 @@ defmodule Quizz21 do
   # Caso base: Si se recibe una lista vacía, retorna la misma lista vacía
   def remove_at([], _i, nueva, _acc), do: nueva
 
-# Pregunta 3
+# Pregunta 3: encode_dup
+
+  # F.P: Si se recibe una lista con elementos, se empieza a llenar una "nueva" lista de tuplas
+  def encode_dup(lista), do: encode_dup(to_charlist(lista), 0, [], hd(to_charlist(lista)))
+
+  #
+  def encode_dup([hd|tl], acc, nueva, anterior) do
+    if (anterior == hd), do: encode_dup(tl, acc + 1, nueva, hd), else: encode_dup(tl, 1, nueva ++ [{acc, anterior}], hd) 
+  end
+
+  # Caso base: Si se recibe una lista vacía, se retorna una lista de tuplas vacía
+  def encode_dup([], acc, nueva, anterior), do: nueva ++ [{acc, anterior}]
 
 end
--- quizz2_1/test/quizz21_test.exs
+++ quizz2_1/test/quizz21_test.exs
@@ -2,6 +2,8 @@ defmodule Quizz21Test do
   use ExUnit.Case
   doctest Quizz21
 
+#Pregunta 1:
+
   #@tag :skip
   test "occurs: counts the number of occurrences of an element in a list" do
     assert Quizz21.occurs([2,3,5,7,11,13,7,5], 11) == 1
@@ -10,12 +12,31 @@ defmodule Quizz21Test do
     assert Quizz21.occurs([2,3,5,7,11,13,7,5], 99) == 0
   end
 
+#Pregunta 2:
+
   #tag :skip
   test "remove_at" do
     assert Quizz21.remove_at([1,2,3], 0) ==  [2,3]
     assert Quizz21.remove_at([4,5,6,7], 2) ==  [4,5,7]
     assert Quizz21.remove_at([4,5,6,7], 10) ==  [4,5,6,7]
     assert Quizz21.remove_at([4,5,6,7], -1) ==  [4,5,6,7]
+  end
+
+#Pregunta 3:
+
+  #tag: skip
+  test "encode_dup: should work with list of numbers" do
+    assert Quizz21.encode_dup([2,2,2,1,2,2,5,5,5,5,7,7]) == [{3,2},{1,1},{2,2},{4,5},{2,7}]
+  end
+
+  #@tag :skip
+  test "encode_dup: should work with list of strings" do
+    assert Quizz21.encode_dup(~w[a a a a b c c a a d e e e e]) == [{4,"a"},{1,"b"},{2,"c"},{2,"a"},{1,"d"},{4,"e"}]
+  end
+
+  #@tag :skip
+  test "encode_dup: should work with characters on a string" do
+    assert Quizz21.encode_dup("aaaabccaadeeee") == [{4,?a},{1,?b},{2,?c},{2,?a},{1,?d},{4,?e}]
   end
 
 end
