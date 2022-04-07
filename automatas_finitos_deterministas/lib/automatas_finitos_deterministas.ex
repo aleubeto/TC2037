d6823d7f0eeedb1f92d7faf7666efab1547d738f
ale_local <alejo.ubeto@gmail.com>
2022-04-07 16:03:31 -0500

    Creación de proyecto de automatas finitos deterministas

---
+++ automatas_finitos_deterministas/lib/automatas_finitos_deterministas.ex
@@ -0,0 +1,18 @@
+defmodule AutomatasFinitosDeterministas do
+  @moduledoc """
+  Documentation for `AutomatasFinitosDeterministas`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AutomatasFinitosDeterministas.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ automatas_finitos_deterministas/test/automatas_finitos_deterministas_test.exs
@@ -0,0 +1,8 @@
+defmodule AutomatasFinitosDeterministasTest do
+  use ExUnit.Case
+  doctest AutomatasFinitosDeterministas
+
+  test "greets the world" do
+    assert AutomatasFinitosDeterministas.hello() == :world
+  end
+end
